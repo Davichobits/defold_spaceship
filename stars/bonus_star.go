33ce081ad77e15a5b8749975fe447925a4651712
David Ruiz <davidruizdeveloper@gmail.com>
2026-06-01 08:59:28 -0500

    initial commit

---
+++ stars/bonus_star.go
@@ -0,0 +1,11 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"bonus_star\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/stars/stars.atlas\"\n"
+  "}\n"
+  ""
+}
